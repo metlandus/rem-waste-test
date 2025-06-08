3698cfab31b1d1d758ec211c7d9cb55441f288e2
Mete Civelek <114185263+metlandus@users.noreply.github.com>
2025-06-08 21:32:22 +0000

    Website is done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Products from './components/Products';
 function App() {
 
   return (
-    <div className="mx-auto p-6 bg-gray-800 text-white">
+    <div className="mx-auto p-6 bg-slate-900 text-white">
       <div className='flex flex-col items-center gap-2 my-8'>
       <h1 className='font-bold text-2xl'>Choose Your Skip Size</h1>
       <p className="text-gray-400 mb-6">Select the right skip for your needs. Click on a skip to see more details.</p>
